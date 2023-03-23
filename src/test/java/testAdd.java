cf889f8b4faecf9543f8ac2a5c5d52263ac2a716
911 Apavaloaei Alexandru-Teodor <72084810+alexteo24@users.noreply.github.com>
2023-03-23 23:56:59 +0200

    Config

--- src/test/java/testAdd.java
+++ src/test/java/testAdd.java
@@ -24,6 +24,7 @@ public class testAdd {
 
     @Test
     public void testAddStudentSuccess(){
+        System.out.println("Testul a fost rulat");
         assertEquals(1,service.saveStudent("50", "Mihailescu", 931));
         service.deleteStudent("50");
     }
